ad59316ade71ceb0abfd73cff912f7adc7a12aff
Administrator <admin@example.com>
2021-09-15 01:03:13 +0800

    add test

---
+++ ActionDemoUnitTest/UnitTest1.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ActionDemoUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [DataTestMethod]
+        [DataRow(1, 1, 2)]
+        [DataRow(12, 30, 42)]
+        [DataRow(14, 1, 15)]
+        public void Test_Add(int a, int b, int expected)
+        {
+            var actual = a + b;
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
