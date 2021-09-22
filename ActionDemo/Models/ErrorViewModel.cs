91f9a63ffd08aa13b6e869ae4928c8610663be74
Duran Hsieh <dog0416@gmail.com>
2021-09-23 00:44:41 +0800

    Update ErrorViewModel.cs

--- ActionDemo/Models/ErrorViewModel.cs
+++ ActionDemo/Models/ErrorViewModel.cs
@@ -7,5 +7,8 @@ namespace ActionDemo.Models
         public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        
+        public string  azure_devops_personal_access_token = "123456789";
+        public string Token = "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";
     }
 }
